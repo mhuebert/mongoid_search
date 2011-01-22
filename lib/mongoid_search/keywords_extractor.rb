9bfd6f77199face242e5f8dc8d66dd8fc7e07c47
Matthew Huebert <mhuebert@gmail.com>
2011-01-22 13:57:06 -0700

    Added support for indexing array fields

--- lib/mongoid_search/keywords_extractor.rb
+++ lib/mongoid_search/keywords_extractor.rb
@@ -1,7 +1,13 @@
 require 'fast_stemmer'
 class KeywordsExtractor
   def self.extract(text)
-    return [] if text.blank?
-    text.mb_chars.normalize(:kd).to_s.gsub(/[^\x00-\x7F]/,'').downcase.split(/[\s\.\-_:;'",]+/).collect { |word| word.stem }
+    # return [] if text.blank?
+    keywords = []
+    [*text].each do |text|
+      text.mb_chars.normalize(:kd).to_s.gsub(/[^\x00-\x7F]/,'').downcase.split(/[\s\.\-_:;'",]+/).each do |word|
+        keywords << word.stem
+      end
+    end
+    keywords
   end
 end
